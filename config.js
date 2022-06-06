7ae096ff44991c858dc35847ad5454ce420e1f42
chimdj <106983645+chimdj@users.noreply.github.com>
2022-06-06 08:53:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Demo",
+    introTitle: 'Bae à;-;;!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    Lạc vào tim 2:>`,
+    btnIntro: 'ehe:33',
+    title: '>.<Phải chăng em đã yêu ngay từ cái nhìn đầu tiên;-;? 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy:< ',
+    btnYes: 'Đúng z iu 2:3',
+    btnNo: 'Không 2 à :>',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao e lại iu 2 <3',
+    btnReply: 'Gửi cho 2 iu dấu :33',
+    reply: 'Vì e iu 2 nhất:3333333',
+    mess: '2 bik mak:3 🥰. Iu e nhất>_< 😘😘',
+    messDesc: 'Đến lúc gặp 2 đói kêu 2 mua cho e ăn nha bae:33.',
+    btnAccept: 'Okiiiii lun 2 iu :3',
+    messLink: 'https://www.facebook.com/profile.php?id=100073236192720' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
